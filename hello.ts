ace337eb526a4cf4d4cb9767659d4cd9f248c084
wangying <leosmilence@163.com>
2019-03-22 17:40:20 +0800

    基础类型

---
+++ hello.ts
@@ -0,0 +1,20 @@
+// 基础类型
+let isDone: boolean = false;
+let decLiteral: number = 6;
+let hexLiteral: number = 0xf00d;
+let binaryLiteral: number = 0b1010;
+let octalLiteral: number = 0o744;
+let names: string = "bob";
+let age: number = 37;
+let sentence: string = `Hello, my name is ${names}.`
+
+console.log({
+    isDone,
+    decLiteral,
+    hexLiteral,
+    binaryLiteral,
+    octalLiteral,
+    names,
+    age,
+    sentence,
+})
